9d9be8d0dd6b37ee6ff08ad4265d71f36edb3e6f
matteo2437 <matteo.budriesi@gmail.com>
2021-10-10 17:28:52 +0200

    add table to pop up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 /*global chrome*/
 import { Grid } from '@mui/material';
-import Button from '@mui/material/Button';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import React, { useEffect } from 'react';
 
 function App() {
+  const [data, setData] = React.useState([]);
+
+  useEffect(() => {
+    chrome.storage.local.get(['urls'], result => {
+      if(!result.urls)
+        return setData([])
+
+      setData(result.urls)
+    })
+  }, [])
+
   return (
     <Grid
       container
@@ -10,7 +28,7 @@ function App() {
       justify="center"
       alignItems="center"
       sx={{
-        width: 200,
+        width: 650,
         height: 400,
       }}
     >
@@ -19,15 +37,33 @@ function App() {
         container 
         justifyContent="center"
       >
-        <Button
-          onClick={() => {
-            chrome.tabs.query({currentWindow: true, active: true}, tabs => {
-              chrome.tabs.sendMessage(tabs[0].id, tabs[0].url)
-            })
-          }}
-        >
-          test
-        </Button>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Url</TableCell>
+            <TableCell align="right">Sessions</TableCell>
+            <TableCell align="right">Total Time</TableCell>
+            <TableCell align="right">Average Time</TableCell>
+            <TableCell align="right">Last Session Time</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.url}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{row.url}</TableCell>
+              <TableCell align="right">{row.sessions}</TableCell>
+              <TableCell align="right">{row.times.totalTime.time}</TableCell>
+              <TableCell align="right">{row.times.averageTime.time}</TableCell>
+              <TableCell align="right">{row.times.lastSessionTime.time}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
       </Grid>
     </Grid>
   );
