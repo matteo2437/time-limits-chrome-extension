f9ced1938fc5bdda7fd8d79ff7d4e94cb8524c4c
matteo2437 <matteo.budriesi@gmail.com>
2021-10-11 20:24:54 +0200

    add get difference in days

--- public/lib/TimeUtilities.js
+++ public/lib/TimeUtilities.js
@@ -3,16 +3,22 @@ export default class TimeUtilities {
     const date = new Date();
     return date.getTime();
   }
+
+  static getDifferenceInDays = (d1, d2) => {
+    const timeDifference = d1 - d2;
+    return Math.floor(timeDifference / (1000 * 3600 * 24))
+  }  
   
   static msToTime = (ms) => {
     const seconds = (ms / 1000).toFixed(1);
     const minutes = (ms / (1000 * 60)).toFixed(1);
     const hours = (ms / (1000 * 60 * 60)).toFixed(1);
     const days = (ms / (1000 * 60 * 60 * 24)).toFixed(1);
-    if (seconds < 60) return seconds + " Sec";
+    
+    if      (seconds < 60) return seconds + " Sec";
     else if (minutes < 60) return minutes + " Min";
-    else if (hours < 24) return hours + " Hrs";
-    else return days + " Days"
+    else if (hours < 24)   return hours + " Hrs";
+    else                   return days + " Days"
   }
 
   static getTimeObj = (ms) => {
